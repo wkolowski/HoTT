cfde99ca0303f54ec52dcfb1064e0090f95c1874
wkolowski <wkolowski@protonmail.com>
2018-04-05 23:25:52 +0200

    Chapter 1 continued.

--- Ch1.v
+++ Ch1.v
@@ -1,7 +1,29 @@
+(** * 1.3 Universes and families *)
+
+Notation "'U'" := Type.
+
+(** * 1.4 Dependent function types (Π-types) *)
+
 Notation "A -> B" :=
   (forall _ : A, B) (at level 99, right associativity, B at level 200).
 
-Inductive prod (A B : Type) : Type :=
+Definition swap {A B C : U} (f : A -> B -> C) : B -> A -> C :=
+  fun (b : B) (a : A) => f a b.
+
+(** * Equality *)
+
+Inductive eq {A : U} (x : A) : A -> U :=
+    | refl : eq x x.
+
+Arguments refl {A} _.
+
+Notation "x = y" := (eq x y) (at level 70, no associativity).
+
+Ltac refl := apply refl.
+
+(** * 1.5 Product types *)
+
+Inductive prod (A B : U) : U :=
     | pair : A -> B -> prod A B.
 
 Arguments pair {A B} _ _.
@@ -11,34 +33,55 @@ Notation "A * B" :=
 
 Notation "( x , y , .. , z )" := (pair .. (pair x y) .. z).
 
-Inductive unit : Type :=
-    | tt : unit.
-
-Definition pr1 {A B : Type} (x : A * B) : A :=
+Definition pr1 {A B : U} (x : A * B) : A :=
 match x with
     | (a, _) => a
 end.
 
-Definition pr2 {A B : Type} (x : A * B) : B :=
+Definition pr2 {A B : U} (x : A * B) : B :=
 match x with
     | (_, b) => b
 end.
 
-Inductive eq {A : Type} (x : A) : A -> Type :=
-    | refl : eq x x.
+Definition prod_rec' {A B C : U}
+  (f : A -> B -> C) (p : A * B) : C :=
+match p with
+    | (a, b) => f a b
+end.
 
-Arguments refl {A} _.
+Definition pr1_rec {A B : U} (x : A * B) : A :=
+  prod_rec' (fun a _ => a) x.
 
-Notation "x = y" := (eq x y) (at level 70, no associativity).
-
-Ltac refl := apply refl.
+Definition pr2_rec {A B : U} (x : A * B) : B :=
+  prod_rec' (fun _ b => b) x.
 
 Lemma prod_uniq :
-  forall (A B : Type) (x : A * B),
+  forall (A B : U) (x : A * B),
     (pr1 x, pr2 x) = x.
 Proof.
   destruct x. cbn. refl.
 Defined.
+
+Definition prod_ind' {A B : U} {C : A * B -> U}
+  (f : forall (a : A) (b : B), C (a, b)) (x : A * B) : C x :=
+match x with
+    | (a, b) => f a b
+end.
+
+(** ** [unit] *)
+
+Inductive unit : U :=
+    | tt : unit.
+
+Definition unit_rec' {C : U} (c : C) (u : unit) : C :=
+match u with
+    | tt => c
+end.
+
+Definition unit_ind' {C : unit -> U} (c : C tt) (u : unit) : C u :=
+match u with
+    | tt => c
+end.
 
 Lemma unit_uniq :
   forall u : unit, u = tt.
@@ -46,7 +89,9 @@ Proof.
   destruct u. refl.
 Defined.
 
-Inductive sigma {A : Type} (B : A -> Type) : Type :=
+(** * 1.6 Dependent pair types (Σ-types) *)
+
+Inductive sigma {A : U} (B : A -> U) : U :=
     | dpair : forall x : A, B x -> sigma B.
 
 Arguments dpair {A B} _ _.
@@ -59,4 +104,36 @@ Arguments dpair {A B} _ _.
 Notation "{ x : A & B }" := (@sigma A (fun x => B))
   (at level 0, x at level 99, right associativity).
 
-Check forall A : Type, {x : A & x = x}.
+Notation "(( x , y , .. , z ))" := (dpair .. (dpair x y) .. z).
+
+Definition pr1' {A : U} {B : A -> U} (x : {a : A & B a}) : A :=
+match x with
+    | ((a, _)) => a
+end.
+
+Definition pr2' {A : U} {B : A -> U} (x : {a : A & B a}) : B (pr1' x) :=
+match x with
+    | ((_, b)) => b
+end.
+
+Definition sigma_rec'
+  {A : U} {B : A -> U} {C : U}
+  (f : forall x : A, B x -> C) (x : {a : A & B a}) : C :=
+match x with
+    | ((a, b)) => f a b
+end.
+
+Definition sigma_ind'
+  {A : U} {B : A -> U} {C : {a : A & B a} -> U}
+  (f : forall (a : A) (b : B a), C ((a, b))) (x : {a : A & B a}) : C x :=
+match x with
+    | ((a, b)) => f a b
+end.
+
+Lemma ac :
+  forall (A B : U) (R : A -> B -> U),
+    (forall a : A, {b : B & R a b}) ->
+      {f : A -> B & forall a : A, R a (f a)}.
+Proof.
+  intros A B R f.
+  
