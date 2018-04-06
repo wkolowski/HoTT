8c8ed71bdf6222264aae41091f2eafc8aff53d80
wkolowski <wkolowski@protonmail.com>
2018-04-06 22:58:05 +0200

    Nearly finished chapter 1.

--- Ch1.v
+++ Ch1.v
@@ -10,7 +10,7 @@ Notation "A -> B" :=
 Definition swap {A B C : U} (f : A -> B -> C) : B -> A -> C :=
   fun (b : B) (a : A) => f a b.
 
-(** * Equality *)
+(** * 1.12 Identity types *)
 
 Inductive eq {A : U} (x : A) : A -> U :=
     | refl : eq x x.
@@ -20,6 +20,12 @@ Arguments refl {A} _.
 Notation "x = y" := (eq x y) (at level 70, no associativity).
 
 Ltac refl := apply refl.
+
+Definition indiscernability
+  {A : U} {C : A -> U} {x y : A} (p : x = y) (cx : C x) : C y :=
+match p with
+    | refl _ => cx
+end.
 
 (** * 1.5 Product types *)
 
@@ -104,30 +110,30 @@ Arguments dpair {A B} _ _.
 Notation "{ x : A & B }" := (@sigma A (fun x => B))
   (at level 0, x at level 99, right associativity).
 
-Notation "(( x , y , .. , z ))" := (dpair .. (dpair x y) .. z).
+Notation "(| x , y , .. , z |)" := (dpair .. (dpair x y) .. z).
 
 Definition pr1' {A : U} {B : A -> U} (x : {a : A & B a}) : A :=
 match x with
-    | ((a, _)) => a
+    | (|a, _|) => a
 end.
 
 Definition pr2' {A : U} {B : A -> U} (x : {a : A & B a}) : B (pr1' x) :=
 match x with
-    | ((_, b)) => b
+    | (|_, b|) => b
 end.
 
 Definition sigma_rec'
   {A : U} {B : A -> U} {C : U}
   (f : forall x : A, B x -> C) (x : {a : A & B a}) : C :=
 match x with
-    | ((a, b)) => f a b
+    | (|a, b|) => f a b
 end.
 
 Definition sigma_ind'
   {A : U} {B : A -> U} {C : {a : A & B a} -> U}
-  (f : forall (a : A) (b : B a), C ((a, b))) (x : {a : A & B a}) : C x :=
+  (f : forall (a : A) (b : B a), C (|a, b|)) (x : {a : A & B a}) : C x :=
 match x with
-    | ((a, b)) => f a b
+    | (|a, b|) => f a b
 end.
 
 Lemma ac :
@@ -136,4 +142,175 @@ Lemma ac :
       {f : A -> B & forall a : A, R a (f a)}.
 Proof.
   intros A B R f.
-  
+  apply (dpair (fun a : A => pr1' (f a))).
+  apply (fun a : A => pr2' (f a)).
+Defined.
+
+Definition Magma : U :=
+  {A : U & A -> A -> A}.
+
+Definition PointedMagma : U :=
+  {A : U & (A -> A -> A) * A}.
+
+(** * 1.7 Coproduct types *)
+
+Inductive coprod (A B : U) : U :=
+    | inl : A -> coprod A B
+    | inr : B -> coprod A B.
+
+Arguments inl {A B} _.
+Arguments inr {A B} _.
+
+Notation "A + B" := (coprod A B) (at level 50, left associativity).
+
+Definition coprod_rec'
+  {A B C : U} (f : A -> C) (g : B -> C) (x : A + B) : C :=
+match x with
+    | inl a => f a
+    | inr b => g b
+end.
+
+Definition coprod_ind'
+  {A B : U} {C : A + B -> U}
+  (f : forall a : A, C (inl a))
+  (g : forall b : B, C (inr b))
+  (x : A + B) : C x :=
+match x with
+    | inl a => f a
+    | inr b => g b
+end.
+
+(** ** [empty] *)
+
+Inductive empty : U := .
+
+Definition empty_rec' {C : U} (x : empty) : C :=
+  match x with end.
+
+Definition empty_ind' {C : empty -> U} (x : empty) : C x :=
+  match x with end.
+
+(** * 1.8 The type of booleans *)
+
+Inductive bool : U :=
+    | true : bool
+    | false : bool.
+
+Definition bool_rec'
+  {C : U} (c1 c2 : C) (b : bool) : C :=
+    if b then c1 else c2.
+
+Definition bool_ind'
+  {C : bool -> U} (ct : C true) (cf : C false) (b : bool) : C b :=
+match b with
+    | true => ct
+    | false => cf
+end.
+
+Lemma bool_dec :
+  forall b : bool,
+    (b = true) + (b = false).
+Proof.
+  destruct b.
+    apply (inl (refl true)).
+    apply (inr (refl false)).
+Defined.
+
+(** We could have done:
+    - A + B := {x : bool & bool_rec' A B x}
+    - A * B := forall x : bool, bool_rec' A B x
+*)
+
+(** * 1.9 The natural numbers *)
+
+Inductive N : U :=
+    | O : N
+    | S : N -> N.
+
+Notation "0" := O.
+
+Fixpoint double (n : N) : N :=
+match n with
+    | 0 => 0
+    | S n' => S (S (double n'))
+end.
+
+Fixpoint add (n m : N) : N :=
+match n with
+    | 0 => m
+    | S n' => S (add n' m)
+end.
+
+Fixpoint N_rec' {C : U} (c0 : C) (cS : N -> C -> C) (n : N) : C :=
+match n with
+    | 0 => c0
+    | S n' => cS n' (N_rec' c0 cS n')
+end.
+
+Fixpoint N_ind'
+  (C : N -> U) (c0 : C 0)
+  (cS : forall n : N, C n -> C (S n))
+  (n : N) : C n :=
+match n with
+    | 0 => c0
+    | S n' => cS n' (N_ind' C c0 cS n')
+end.
+
+Lemma double_rec :
+  forall n : N,
+    double n =
+    N_rec' 0 (fun _ n => S (S n)) n.
+Proof.
+  apply N_ind'; cbn.
+    refl.
+    intros. rewrite H. refl.
+Defined.
+
+Lemma add_rec :
+  forall n m : N,
+    add n m =
+    N_rec' (fun n => n) (fun _ f n => S (f n)) n m.
+Proof.
+  apply
+    (N_ind'
+      (fun n : N => forall m : N,
+        add n m =
+        N_rec' (fun n : N => n) (fun _ f n => S (f n)) n m));
+  cbn; intros.
+    refl.
+    rewrite H. refl.
+Defined.
+
+Lemma assoc :
+  forall i j k : N,
+    add i (add j k) = add (add i j) k.
+Proof.
+  apply
+    (N_ind'
+      (fun i : N =>
+        forall j k : N, add i (add j k) = add (add i j) k));
+  cbn; intros.
+    refl.
+    rewrite H. refl.
+Defined.
+
+(** * 1.11 Propositions as types *)
+
+Definition not (A : U) := A -> empty.
+
+Notation "~ A " := (not A) (at level 75, right associativity).
+
+Lemma and_not :
+  forall A B : U,
+    (~ A) * (~ B) -> ~ (A + B).
+Proof.
+  destruct 1 as [na nb]. destruct 1 as [a | b].
+    apply na, a.
+    apply nb, b.
+Defined.
+
+Definition le (n m : N) : U :=
+  {k : N & add n k = m}.
+
+Definition lt (n m : N) : U :=
+  {k : N & add n (S k) = m}.
